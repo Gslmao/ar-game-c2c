5498c0ab12090e2a78f47bd914acbf43c96b2834
Gslmao <seriouscubist@gmail.com>
2026-09-07 15:57:20 +0530

    feat: about page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left text-5xl">
+            All the best for us for the hackathon
+            <h1>Deployed this to vercel and it works fine lmao.</h1>
+            <h1>
+              Made with &#9829; by Anibhav and Guru
+            </h1>
+          </div>
+      </main>
+    </div>
+  );
+}
